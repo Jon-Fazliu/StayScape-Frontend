2ad632fedcf0ca6abae00c16e0a20d48c798e22c
DorentinaBerisha <berishadorentina@outlook.com>
2024-04-16 20:04:52 +0100

    Creating a hotels search view

--- stay-scape/src/List.jsx
+++ stay-scape/src/List.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import "./list.css";
+import Navbar from "./Navbar";
+import Header from "./Header";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
 
 const List = () => {
-  return <div> List </div>;
+  const location = useLocation();
+  const destination = ""
+  const date = ""
+  const [openDate, setOpenDate] = useState(false);
+  const options = ""
+
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input
+                placeholder={destination}
+                type="text"
+                onChange={(e) => console.log("Changed destination")}
+              />
+            </div>
+            <div className="lsItem">
+              <label>Check-in Date</label>
+              <span onClick={() =>  console.log("Changed check in date")}>
+              </span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => console.log("Changed date")}
+                  minDate={new Date()}
+                  ranges={date}
+                />
+              )}
+            </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min Price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Max Price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adult</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={"options.adult"}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children</span>
+                  <input
+                    type="number"
+                    min={0}
+                    className="lsOptionInput"
+                    placeholder={"options.children"}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Room</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={"options.room"}
+                  />
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default List;
